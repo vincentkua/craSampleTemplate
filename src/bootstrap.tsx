ac3e0e7fd666da27bb3dfe6ebb0dfb5f02779dc9
vincentkua <zylyn89@gmail.com>
2024-01-15 22:53:03 +0800

    init: initiated with cra template

---
+++ src/bootstrap.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import Providers from "./Providers";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<Providers />);
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <h3>Home Page</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+        excepturi dicta perspiciatis officia animi unde neque incidunt,
+        blanditiis alias consequatur ut quod odio laudantium quaerat
+        voluptatibus nostrum in fugiat accusantium.
+      </p>
+    </>
+  );
+};
+
+export default Home;
